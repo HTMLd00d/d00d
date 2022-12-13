f63507193a3fc87acb3d83fdfb0c479bcf759db0
d00dez <statik19@yandex.ru>
2022-12-13 15:30:24 +0500

    02

---
+++ к семинару 4/дз02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int Sum(int num)
+{
+    int result = 0;
+    result += num % 10;
+    num /= 10;
+    if (num > 0)
+    {
+        result += Sum(num);
+    }
+    return result;
+}
+int sum = Sum(number);
+Console.WriteLine($"Сумма чисел равна {sum}");
